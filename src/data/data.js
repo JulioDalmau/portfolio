38a9c5021a8e254cfa01446b7ec7e0cf9f84da09
JulioDalmau <dalmau.ju@gmail.com>
2023-02-15 17:17:09 -0300

    Add data

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+const data = {
+  es: {
+    navbar: ["Sobre mí", "Proyectos", "Technologías", "Contacto"],
+    init: [
+      "Hola, mi nombre es",
+      "Julio Dalmau.",
+      "Soy",
+      [
+        "Full Stack Developer.",
+        "MERN Developer.",
+        "PERN Developer.",
+        "Node.js Developer.",
+        "React.js Developer.",
+      ],
+      'Soy un desarrollador web, me gusta crear aplicaciones, trabajar en equipo y experimentar con nuevas tecnologías '
+    ],
+    aboutMe: [
+        'Desde niño siempre fui un apasionado por la tecnología. Durante 12 años trabajé en el mundo IT como soporte técnico y coordinador de soporte técnico en grandes empresas, esto me brindó experiencia de trabajo en equipo, en condiciones de gran variabilidad tecnológica y de alta demanda de solicitudes.',
+        'Durante el ultimo año tomé una nueva dirección laboral orientada al desarrollo web. Para ello, completé un Bootcamp el cual me fortaleció mis habilidades técnicas y blandas.',
+        'Descargar CV',
+    ],
+    Projects: {
+        title: 'Proyectos',
+        projects: [
+            {
+                name: '',
+                path: '',
+                linkGithub: '',
+                linkWeb: '',
+                description: '',
+                skills: [''],
+
+            }
+        ]
+    },
+    skills: {
+        title: 'Tecnologías',
+        skills: ['']
+    },
+    contact: {
+        title: 'Contacto',
+        button: 'Contactame'
+    },
+    footer: {
+        copyright: "Sitio desarrollado por Julio Dalmau 2023."
+    }
+  },
+};
+
+export default data
