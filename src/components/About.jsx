14bf89ae21fd759dd893baef7ca184eb1615c1ac
JulioDalmau <dalmau.ju@gmail.com>
2023-02-17 23:18:49 -0300

    finish about & projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import data from '../data/data'
-import profilePicture from '../assets/jdPicture.png'
+import React from "react";
+import data from "../data/data";
+import profilePicture from "../assets/jdPicture.png";
+import julioDalmau from '../assets/about/julioDalmau.pdf'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#0A0C0D] text-[#F2F2F2]'>
-        <div className='flex flex-col justify-center items-center w-full h-full pt-10'>
-            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-                <div className='sm:text-right pb-8 pl-4'>
-                    <p className='text-4xl font-bold inline border-b-4 border-[#115959] whitespace-nowrap'>Sobre mí</p>
-                </div>
-            <div></div>
-            </div>
-            <div className='max-w-[1000px] w-full grid md:grid-cols-2 gap-8 px-4'>
-                <div className='md:justify-end md:items-end mx-auto'> 
-                    <img className='w-[300px] ' src={profilePicture} alt="foto perfil" />
-                </div>
-                <div>
-                    <p className='px-4'>{data.es.aboutMe[0]}</p>
-                    <p className='px-4'>{data.es.aboutMe[1]}</p>
-                </div>
-            </div>
+    <div name="about" className="w-full h-screen bg-[#0A0C0D] text-[#F2F2F2] max-sm:pb-24">
+      <div className="flex flex-col justify-center items-center w-full h-full pt-10">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-left pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-[#115959] whitespace-nowrap font-montserrat">
+              Sobre mí
+            </p>
+          </div>
+          <div></div>
         </div>
+        <div className="max-w-[1000px] w-full grid md:grid-cols-2 gap-8 px-4">
+          {/* <div className="md:justify-end md:items-end mx-auto"> */}
+          <div className="md:justify-start md:ml-0 mx-auto">
+            <img
+              className="w-[300px] "
+              src={profilePicture}
+              alt="foto perfil"
+            />
+          </div>
+          <div>
+            <p className="px-4 font-roboto">{data.es.aboutMe[0]}</p>
+            <p className="px-4 font-roboto">{data.es.aboutMe[1]}</p>
+            <div className="justify-self-center text-center pt-4">
+                <a href={julioDalmau} type="application/pdf" target="_blank" rel="noreferrer" >
+              <button className="text-[#F2F2F2] font-semibold py-3 px-3 border-2 my-2 hover:bg-[#2EF2BB] hover:border-[#2EF2BB] hover:text-[#0A0C0D] hover:font-semibold font-montserrat">
+                {data.es.aboutMe[2]}
+              </button>
+                </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import data from "../data/data";
+
+
+const Projects = () => {
+
+    const {projects} = data.es.Projects
+    
+  return (
+    <div name="projects" className="w-full md:h-screen text-[#F2F2F2] bg-[#0A0C0D] max-sm:pb-28 max-sm:pt-28">
+      <div className="max-w-[1000px] mx-auto px-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-[#F2F2F2] border-[#115959] font-montserrat">{data.es.Projects.title}</p>  
+        </div>
+
+      {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+
+            {/* Items */}
+                {projects.map((project, index) => (
+          <div key={index} 
+          style={{backgroundImage: `url(${project.path})`}}
+          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-works font-montserrat">
+
+            {/* hover */}
+            <div className="opacity-0 group-hover:opacity-100">
+               <span className="text-2xl font-bold text-[#F2F2F2] tracking-wider">
+               {project.name}
+               </span>
+               <div className="pt-8 text-center">
+                <a href={project.linkGithub} target='_blank' rel="noreferrer">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-[#F2F2F2] text-[#040c16] font-bold text-lg">
+                        Github
+                    </button>
+                </a>
+                {/* <a href={project.linkWeb} target="_blank">
+                    <button className="text-center rounded-lg px-4 py-3 m-2
+                       bg-[#F2F2F2] text-[#040c16] font-bold text-lg">
+                        Website
+                       </button>
+                </a> */}
+               </div>
+            </div>
+            </div>))}
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
