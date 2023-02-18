ae30155fc2d10749fc8e215a0f710984a6e8e3bd
JulioDalmau <dalmau.ju@gmail.com>
2023-02-17 21:19:41 -0300

    finish app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import Navbar from './components/Navbar';
-import Home from './components/Home'
-
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-  
-
-
-
   return (
     <>
-        <Navbar/>
-        <Home />
-        
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </>
   );
 }
