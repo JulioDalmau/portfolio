5f28364756d3c5778ec568fb2668dd97ebffd2b2
JulioDalmau <dalmau.ju@gmail.com>
2023-02-18 23:10:13 -0300

    change typed effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import data from "../data/data";
-import Typed from "react-typed";
 import { Link } from "react-scroll";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
@@ -13,19 +13,24 @@ const Home = () => {
         <h1 className="text-4xl sm:text-7xl font-bold text-[#4429F2] font-montserrat">
           {data.es.init[1]}
         </h1>
-        <h2 className="text-[28px] md:text-3xl font-bold text-[#7D79F2] font-roboto">
+
+        <h2 className="text-[28px] md:text-3xl flex flex-wrap font-bold text-[#7D79F2] font-roboto">
           {data.es.init[2]}
-          <Typed
-            strings={data.es.init[3]}
-            typeSpeed={150}
-            backSpeed={50}
-            loop
-            showCursor={false}
-            className="text-[#5550F2] font-montserrat font-bold"
-          />{" "}
+          <h2 className="ml-2 mr-1 text-[#5550F2] font-roboto font-bold justify-center">
+            <Typewriter
+              options={{
+                strings: data.es.init[3],
+                autoStart: true,
+                loop: true,
+                cursor: "",
+              }}
+            />
+          </h2>
           Developer
         </h2>
-        <p className="text-[#7D79F2] py-4 max-w-[400px] font-montserrat font-semibold">{data.es.init[4]}</p>
+        <p className="text-[#7D79F2] py-4 max-w-[400px] font-montserrat font-semibold">
+          {data.es.init[4]}
+        </p>
         <div className="mt-5">
           <Link
             className="text-[#F2F2F2] font-semibold py-3 px-3 border-2 hover:bg-[#4429F2] hover:border-[#4429F2] hover:text-[#0A0C0D] hover:font-semibold cursor-pointer"
